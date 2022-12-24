22fbcf29aa79634ced9547ab501f2ac6c56ef326
samay-rgb <rahuldba99@gmail.com>
2022-12-24 12:21:40 +0530

    minor change

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def get_movie_similarity(imdb_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
+    app.run(debug=True, port=os.getenv('PORT', 5000))
